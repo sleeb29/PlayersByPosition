5b242bd75634150e08ad9f675e4ec65ddb7708af
sleeb29 <sfleeb@gmail.com>
2018-02-28 21:39:01 -0500

    first commit for file

---
+++ src/main/java/com/baseball/players_by_position/service/PlayerService.java
@@ -0,0 +1,14 @@
+package com.baseball.players_by_position.service;
+
+import com.baseball.players_by_position.model.model.Player;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public interface PlayerService {
+
+    void populate(List<Player> players);
+
+    HashMap<String, Set<Player>> getPositionToStartingPlayersMap();
+}
